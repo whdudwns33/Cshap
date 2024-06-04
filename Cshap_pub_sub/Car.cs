1f0e61d9bdb7de9b11f1cf0cbf21b21451400190
jyj051811@gmail.com <jyj051811@gmail.com>
2024-06-04 21:32:47 +0900

    20240604 pus-sub first

---
+++ Cshap_pub_sub/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cshap_pub_sub
+{
+    class Car
+    {
+        public string CarName { get; set; }
+        public string DoSomething { get; set; }
+
+        // 생성자 사용
+        public Car(string carName, string doSomething)
+        {
+            this.CarName = carName;
+            this.DoSomething = doSomething;
+        }
+    }
+}
---
+++ Cshap_pub_sub/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    // 이벤트 선언
+    public event EventHandler<string> SomethingHappened;
+
+    // 실행
+    static void Main(string[] args)
+    {
+        Console.Write("Person의 이름을 입력하세요 : ");
+        string name = Console.ReadLine();
+        Console.Write("Person의 나이를 입력하세요 : ");
+        string age = Console.ReadLine();
+        Console.Write("Person의 type을 입력하세요 : ");
+
+    }
+
+
+    // Publisher
+    public void onSonmethingHappend(object sender, string something)
+    {
+        SomethingHappened?.Invoke(this, something);
+    }
+
+
+}
