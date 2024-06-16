49724003856771b8326c33fd49e46890bde50596
jyj051811@gmail.com <jyj051811@gmail.com>
2024-06-16 23:19:57 +0900

    20240616

---
+++ Cs_pr01/Controller/PostController.cs
@@ -0,0 +1,27 @@
+﻿using Cs_pr01.Model;
+using Cs_pr01.Sql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Cs_pr01.Controller
+{
+    public class PostController
+    {
+        private BoardContext db = new BoardContext();
+
+        public List<Post> GetPosts()
+        {
+            return db.GetAllPosts();
+        }
+
+        public void AddPost(Post post)
+        {
+            db.AddPost(post);
+        }
+    }
+}
+
---
+++ Cs_pr01/Sql/BoardSql.cs
@@ -0,0 +1,56 @@
+﻿using Cs_pr01.Model;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cs_pr01.Sql
+{
+    public class BoardContext
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["board"].ConnectionString;
+
+        public List<Post> GetAllPosts()
+        {
+            List<Post> posts = new List<Post>();
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM Posts", connection);
+
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Post post = new Post
+                        {
+                            PostID = reader.GetInt32("PostID"),
+                            Title = reader.GetString("Title"),
+                            Content = reader.GetString("Content"),
+                            CreatedAt = reader.GetDateTime("CreatedAt")
+                        };
+                        posts.Add(post);
+                    }
+                }
+            }
+
+            return posts;
+        }
+
+        public void AddPost(Post post)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                MySqlCommand cmd = new MySqlCommand("INSERT INTO Posts (Title, Content) VALUES (@Title, @Content)", connection);
+                cmd.Parameters.AddWithValue("@Title", post.Title);
+                cmd.Parameters.AddWithValue("@Content", post.Content);
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
